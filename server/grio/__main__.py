89aa142c3f65d6ea38494cf9fbc51781a2443a4f
John Nolette <john@neetgroup.net>
2018-02-05 17:00:43 -0500

    Added basic docker files, added basic make

--- server/grio/__main__.py
+++ server/grio/__main__.py
@@ -1,6 +1,6 @@
-from db import drop_database, bootstrap
-from constants import APP_HOST, APP_PORT, BOOTSTRAP
-from api import app
+from grio.db import drop_database, bootstrap
+from grio.constants import APP_HOST, APP_PORT, BOOTSTRAP
+from grio.api import app
 
 
 if __name__ == '__main__':
