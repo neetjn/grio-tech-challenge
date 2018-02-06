8ca6bc59635bd279fd4eca2272b24c2dbb5be9d7
John Nolette <john@neetgroup.net>
2018-02-05 20:01:13 -0500

    implemented animejs

--- client/router/routes/Home.vue
+++ client/router/routes/Home.vue
@@ -5,7 +5,7 @@
         <div class="container">
           <div class="navbar-brand">
             <a class="navbar-item" href="https://github.com/neetjn/grio-tech-challengen">
-              <i class="ico ico-left ico-xxl fi-social-github fi-2x"></i> <span class="logo-text">Fork Me</span>
+              <i class="ico ico-left ico-xl fi-social-github fi-2x"></i> <span class="logo-text">Fork</span>
             </a>
             <span class="navbar-burger burger" data-target="navbarMenuHeroA">
               <span></span>
@@ -15,13 +15,13 @@
           </div>
           <div id="navbarMenuHeroA" class="navbar-menu">
             <div class="navbar-end">
-              <a class="navbar-item">
+              <a class="navbar-item" href="https://www.linkedin.com/in/john-nolette-69ba72132/" target="_blank">
                 <i class="ico ico-left ico-xl fi-social-linkedin"></i> Linkedin
               </a>
-              <a class="navbar-item">
+              <a class="navbar-item" href="https://github.com/neetjn" target="_blank">
                 <i class="ico ico-left ico-xl fi-social-github"></i> Github
               </a>
-              <a class="navbar-item">
+              <a class="navbar-item" href="https://twitter.com/neet_jn" target="_blank">
                 <i class="ico ico-left ico-xl fi-social-twitter"></i> Twitter
               </a>
             </div>
@@ -34,7 +34,7 @@
     <div class="hero-body">
       <div class="container has-text-centered">
         <h1 class="title animated fadeInUp">
-          Search
+          Search <i class="ico fi-magnifying-glass animated flip"></i>
         </h1>
         <h2 class="subtitle">
           <SearchBar class="animated fadeInDown" />
@@ -43,18 +43,29 @@
     </div>
 
     <!-- Hero footer: will stick at the bottom -->
-    <div class="hero-foot">
-      <h1 class="has-text-centered">Web App by John Nolette</h1>
+    <div class="hero-foot has-text-centered">
+      <span id="user-count" /> Records
+      <h1>Web App by John Nolette</h1>
+      <img id="vue-logo" src="../../assets/images/vue.svg" />
     </div>
   </section>
 </template>
 
 <script>
   import SearchBar from '@/components/SearchBar.vue'
+  import anime from 'animejs'
 
   export default {
     name: 'Home',
-    components: { SearchBar }
+    components: { SearchBar },
+    mounted: function() {
+      anime({
+        targets: '#user-count',
+        textContent: 1000,
+        round: 1,
+        easing: 'easeInOutExpo'
+      })
+    }
   }
 </script>
 
@@ -67,5 +78,11 @@
   }
   .hero-foot {
     padding-bottom: 2rem;
+  }
+  #vue-logo {
+    width: 5rem;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
   }
 </style>
